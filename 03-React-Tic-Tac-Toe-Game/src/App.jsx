59196fc218070b8cec7118bbb5aa36cdcc9fd992
mattasundar88 <matta.sundar88@gmail.com>
2024-12-08 13:33:21 -0600

    Building the initial layout for two players

--- 03-React-Tic-Tac-Toe-Game/src/App.jsx
+++ 03-React-Tic-Tac-Toe-Game/src/App.jsx
@@ -1,7 +1,23 @@
 function App() {
   return (
-    <p>Coming Soon!</p>
+    <main>
+      <div id="game-container">
+        {/* Players */}
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        {/* GameBoard */}
+      </div>
+      {/* Log Game Activity */}
+    </main>
   )
 }
 
-export default App
+export default App;
