5926e470c121af910df64828659ca14b29823be0
mattasundar88 <matta.sundar88@gmail.com>
2024-12-08 22:23:27 -0600

    Edit and Save Player Names - Two Way Binding

--- 03-React-Tic-Tac-Toe-Game/src/components/Player.jsx
+++ 03-React-Tic-Tac-Toe-Game/src/components/Player.jsx
@@ -1,11 +1,34 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({initialName, symbol}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEdit() {
+    // setIsEditing(!isEditing); Wrong way of updating state
+    // As state updates aren't instantenous, need to pass a function
+    setIsEditing((editing) => !editing);
+  }
+  
+  function handlePlayerNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    // Two-way binding
+    editablePlayerName = <input type='text' required value={playerName} onChange={handlePlayerNameChange}/>;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
-        <span className="player-symbol">{symbol}</span>
+        {editablePlayerName}
+        <span className="player-symbol">
+          {symbol}
+        </span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEdit}> {isEditing ? 'Save' : 'Edit'}</button>
     </li>
   )
 }
